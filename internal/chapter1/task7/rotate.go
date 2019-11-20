a807d0e1194ea7d68a847cf1dcbffcbae81e2c9a
Ivan Kalita <input.txt@hotmail.com>
2019-11-20 22:48:30 +0300

    Better explanation for task 1.7

--- internal/chapter1/task7/rotate.go
+++ internal/chapter1/task7/rotate.go
@@ -1,6 +1,9 @@
 package task7
 
 // Rotate – O(1) memory, O(n^2) time
+// The idea is:
+// 1. iterate over each bound (perimeter) from the outermost to innermost
+// 2. iterate over one side of each bound and rotate 4 elements at once (one for each side of this bound)
 func Rotate(a *[][]int32) {
 	// assumes that len(a) == len(a_i)
 	n := len(*a)
@@ -10,8 +13,12 @@ func Rotate(a *[][]int32) {
 	for side := n; side > 1; side -= 2 {
 		right := left + side - 1
 		for i := left; i < right; i++ {
-			// TODO: explain this formula and refactor
-			(*a)[n-top-1][i], (*a)[n-i-1][n-top-1], (*a)[top][n-i-1], (*a)[i][top] = (*a)[i][top], (*a)[n-top-1][i], (*a)[n-i-1][n-top-1], (*a)[top][n-i-1]
+			// we a going to rotate 4 elements at once
+			x1, y1 := i, top
+			x2, y2 := n-top-1, i
+			x3, y3 := n-i-1, n-top-1
+			x4, y4 := top, n-i-1
+			(*a)[x1][y1], (*a)[x2][y2], (*a)[x3][y3], (*a)[x4][y4] = (*a)[x4][y4], (*a)[x1][y1], (*a)[x2][y2], (*a)[x3][y3]
 		}
 
 		top++
